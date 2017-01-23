4d39c007c632a32a9f3cf4ebbfb0edf12f146d4c
ali khoramshahi <alikhoramshahi@alis-MacBook-Pro.local>
2017-01-23 13:59:20 +0100

    coverage added

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,31 @@
 /* jshint node: true */
 'use strict';
-require('coffee-script/register');
 
-var
-  gulp = require('gulp'),
-  jasmine = require('gulp-jasmine'),
-  reporters = require('jasmine-reporters');
+require('coffee-script/register');
+const istanbul = require('gulp-istanbul')
+const eslint = require('gulp-eslint')
+const mocha = require('gulp-mocha')
+const gulp = require('gulp')
+const jasmine = require('gulp-jasmine')
+const reporters = require('jasmine-reporters')
+
+gulp.task('pre-test', function() {
+  return gulp.src(['src/*.js'])
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire())
+})
+
+gulp.task('cover', ['pre-test'], function() {
+  return gulp.src('test/**/*Spec.{js,coffee}')
+  .pipe(jasmine())
+  .pipe(istanbul.writeReports())
+})
 
 gulp.task('test', function(){
   return gulp.src('test/**/*Spec.{js,coffee}')
-    .pipe(jasmine({
-      reporter: new reporters.TerminalReporter()
-    }));
+  .pipe(jasmine({
+    reporter: new reporters.TerminalReporter()
+  }));
 });
 
 gulp.task('watch', function(){
